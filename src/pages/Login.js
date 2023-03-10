a9f7c215e13e3568e53a9a8124a73c04058cb296
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-10 16:23:38 -0300

    Implement email and password input

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../components/Card';
+import FormGroup from '../components/FormGroup';
 
 function Login() {
     return (
@@ -8,7 +9,31 @@ function Login() {
                 <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
                     <div className="bs-docs-section">
                         <Card title="Login">
-                            
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup label="* Email:" htmlFor="exampleInputEmail">
+                                                <input
+                                                    type="email"
+                                                    className="form-control" 
+                                                    id="exampleInputEmail"
+                                                    aria-describedby="emailHelp" 
+                                                    placeholder="Enter email" 
+                                                />
+                                            </FormGroup>
+                                            <FormGroup label="* Password:" htmlFor="exampleInputPassword">
+                                                <input
+                                                    type="password"
+                                                    className="form-control"
+                                                    id="exampleInputPassword"
+                                                    placeholder="Enter password"
+                                                />
+                                            </FormGroup>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
                         </Card>
                     </div>
                 </div>
