ca53b5bb1ec6a163dd826130198b58067c2ddf20
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-10 13:32:53 -0300

    Refactor some files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import Login from './pages/Login';
+import 'bootswatch/dist/vapor/bootstrap.css';
+
 function App() {
   return (
-    <div>
-      Hello, world!
-    </div>
+    <Login />
   );
 }
 
