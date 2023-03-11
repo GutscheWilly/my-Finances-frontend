c96e5ccf343fb7cf3b5fddc6d1a6405b3ee3988f
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-11 17:30:07 -0300

    Create register and cancel buttons

--- src/pages/RegisterUser.js
+++ src/pages/RegisterUser.js
@@ -9,12 +9,19 @@ function RegisterUser() {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
+    const register = () => {
+        console.log(name);
+        console.log(email);
+        console.log(password);
+        console.log(confirmPassword);
+    }
+
     return (
-        <div className="container">
+        <div className="container" style={{height: '100vh'}}>
             <Card title="Register User">
                 <div className="row">
                     <div className="col-lg-12">
-                        <div className="bs-component">
+                        <div className="bs-component col-lg-6" style={ {position: 'relative', left: '270px'} }>
                             <FormGroup label="* Name:" htmlFor="inputName">
                                 <input
                                     type="text"
@@ -51,6 +58,10 @@ function RegisterUser() {
                                     onChange={ (event) => setConfirmPassword(event.target.value) }
                                 />
                             </FormGroup>
+                        </div>
+                        <div className="btn-group mt-5 d-grid gap-3 col-lg-2" style={ {position: 'relative', left: '450px'} }>
+                            <button onClick={register} className="btn btn-success">Register</button>
+                            <button className="btn btn-danger">Cancel</button>
                         </div>
                     </div>
                 </div>
