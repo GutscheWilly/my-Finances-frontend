c11b8409147e012ced3b8f205b55a063e29ddaeb
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-10 23:44:07 -0300

    Set margin to Form Group component

--- src/components/FormGroup.js
+++ src/components/FormGroup.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function FormGroup({ label, htmlFor, children }) {
     return (
-        <div className="form-group">
-            <label htmlFor={htmlFor} className="form-label mt-2">{label}</label>
+        <div className="form-group mt-2 mb-2">
+            <label htmlFor={htmlFor} className="form-label">{label}</label>
             {children}
         </div>
     );
