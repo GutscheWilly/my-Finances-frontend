855b5ef9da4c84e2e2f710354817840b224f6004
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-13 19:52:50 -0300

    Include Home page path in Routes App

--- src/app/RoutesApp.js
+++ src/app/RoutesApp.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Login from '../pages/Login';
 import RegisterUser from '../pages/RegisterUser';
+import Home from '../pages/Home';
 
 function RoutesApp() {
     return (
@@ -9,6 +11,7 @@ function RoutesApp() {
             <Routes>
                 <Route path="/login" element={ <Login /> } />
                 <Route path="/register-user" element={ <RegisterUser /> } />
+                <Route path="/home" element={ <Home /> } />
             </Routes>
         </BrowserRouter>
     );
