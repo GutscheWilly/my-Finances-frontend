8a11dbe072c4ef1c0b979c86ddcc3f65b75309fe
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-13 19:45:51 -0300

    Add balance state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Home() {
+    const [balance, setBalance] = useState(0);
+
     return (
         <div className="jumbotron">
             <h1 className="display-3">Welcome!</h1>
-            <p className="lead">Your balance to this month is $100</p>
+            <p className="lead">Your balance to this month is ${balance}</p>
             <hr className="my-4"/>
             <p className="lead">
                 <a className="btn btn-danger btn-lg" href="" role="button"><i className="fa fa-users"></i>Add launch</a>
