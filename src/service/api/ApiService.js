3382b1fe4e4434d90b889a5e03c476c435847ddf
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-14 20:39:10 -0300

    Include constructor to Api Service

--- src/service/api/ApiService.js
+++ src/service/api/ApiService.js
@@ -2,24 +2,32 @@ import axios from 'axios';
 
 const httpClient = axios.create({
     baseURL: 'http://localhost:8080'
-})
+});
 
 class ApiService {
 
+    constructor(directory) {
+        this.directory = directory;
+    }
+
+    addPath(url) {
+        return this.directory + url;
+    }
+
     post(url, object) {
-        return httpClient.post(url, object);
+        return httpClient.post(this.addPath(url), object);
     }
 
     put(url, object) {
-        return httpClient.put(url, object);
+        return httpClient.put(this.addPath(url), object);
     }
 
     get(url) {
-        return httpClient.get(url);
+        return httpClient.get(this.addPath(url));
     }
 
     delete(url) {
-        return httpClient.delete(url);
+        return httpClient.delete(this.addPath(url));
     }
 }
 
