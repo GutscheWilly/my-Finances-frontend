9e263c3c732f5addf5a582c29bb188897f9c5cb9
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-15 20:59:35 -0300

    Add user name in Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,21 +6,25 @@ import LocalStorageService from '../service/local-storage/LocalStorageService';
 
 function Home() {
     const [balance, setBalance] = useState();
+    const [userName, setUserName] = useState();
 
     const userService = new UserService();
 
     useEffect(() => {
         const loggedUser = LocalStorageService.getItem('logged_user');
         const id = loggedUser.id; 
+        const name = loggedUser.name;
 
         userService.getBalance(id)
         .then(response => setBalance(response.data))
         .catch(erro => console.log(erro));
+
+        setUserName(name);
     });
 
     return (
         <div className="jumbotron">
-            <h1 className="display-3">Welcome!</h1>
+            <h1 className="display-3">Welcome, {userName}!</h1>
             <p className="lead">Your balance to this month is $ {balance}</p>
             <hr className="my-4"/>
             <p className="lead">
