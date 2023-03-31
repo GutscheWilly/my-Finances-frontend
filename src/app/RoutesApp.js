b81a813deab1db8e19f51b2fd77add9e86af7d1a
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-30 21:08:06 -0300

    Include Register Launch page in Routes App

--- src/app/RoutesApp.js
+++ src/app/RoutesApp.js
@@ -5,6 +5,7 @@ import Login from '../pages/Login';
 import RegisterUser from '../pages/RegisterUser';
 import Home from '../pages/Home';
 import Launch from '../pages/launch/Launch';
+import RegisterLaunch from '../pages/launch/RegisterLaunch';
 
 function RoutesApp() {
     return (
@@ -14,6 +15,7 @@ function RoutesApp() {
                 <Route path="/register-user" element={ <RegisterUser /> } />
                 <Route path="/home" element={ <Home /> } />
                 <Route path="/launch" element={ <Launch /> } />
+                <Route path="/register-launch" element={ <RegisterLaunch /> } />
             </Routes>
         </BrowserRouter>
     );
