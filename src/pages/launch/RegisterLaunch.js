5264de7de867f0955121d234b45cdb5da133710b
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-30 23:13:39 -0300

    Finish interface of Register Launch

--- src/pages/launch/RegisterLaunch.js
+++ src/pages/launch/RegisterLaunch.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Card from '../../components/Card';
 import FormGroup from '../../components/FormGroup';
+import MenuOption from '../../components/MenuOption';
+import { monthsOptionList, launchTypesOptionList } from '../../service/launch/LaunchService';
 
 function RegisterLaunch() {
     return (
         <Card title="Register Launch">
-            <div className="row col-md-12">
+            <div className="row col-md-13">
                 <FormGroup htmlFor="inputDescription" label="* Description:">
                     <input id="inputDescription" type="text" className="form-control" />
                 </FormGroup>
@@ -21,9 +23,34 @@ function RegisterLaunch() {
 
                 <div className="col-md-6">
                     <FormGroup htmlFor="inputMonth" label="* Month:">
-                        <input id="inputMonth" type="text" className="form-control" />
+                        <MenuOption id="inputMonth" options={monthsOptionList} className="form-control" />
                     </FormGroup>
                 </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-4">
+                    <FormGroup htmlFor="inputValue" label="* Value:">
+                        <input id="inputValue" type="text" className="form-control" />
+                    </FormGroup>
+                </div>
+
+                <div className="col-md-4">
+                    <FormGroup htmlFor="inputType" label="* Type:">
+                        <MenuOption id="inputType" options={launchTypesOptionList} className="form-control" />
+                    </FormGroup>
+                </div>
+
+                <div className="col-md-4">
+                    <FormGroup htmlFor="inputStatus" label="Status:">
+                        <input id="inputStatus" type="text" className="form-control" disabled />
+                    </FormGroup>
+                </div>
+            </div>
+
+            <div className="group d-flex justify-content-center mt-4">
+                <button type="button" className="btn btn-success">Register</button>
+                <button type="button" className="btn btn-danger">Cancel</button>
             </div>
         </Card>
     );
