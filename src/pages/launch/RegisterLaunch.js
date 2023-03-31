2e7442e1a4dc2046bf29509ec7d5bac156f42ffe
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-30 21:06:40 -0300

    Create Register Launch page

---
+++ src/pages/launch/RegisterLaunch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Card from '../../components/Card';
+
+function RegisterLaunch() {
+    return (
+        <Card title="Register Launch">
+
+        </Card>
+    );
+}
+
+export default RegisterLaunch;
