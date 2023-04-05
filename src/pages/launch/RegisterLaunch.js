951b4adcd64ed962afd27515bb4c7900e0f99282
Willy Gutsche <willy.gutsche@unifesp.br>
2023-04-04 21:21:19 -0300

    Start to include possible launch to be edit

--- src/pages/launch/RegisterLaunch.js
+++ src/pages/launch/RegisterLaunch.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Card from '../../components/Card';
 import FormGroup from '../../components/FormGroup';
@@ -13,6 +14,8 @@ import LocalStorageService from '../../service/local-storage/LocalStorageService
 
 function RegisterLaunch() {
     const userId = LocalStorageService.getItem('logged_user').id;
+
+    const [launchToBeUpdate, setLaunchToBeUpdate] = useState({});
 
     const [description, setDescription] = useState();
     const [year, setYear] = useState();
@@ -44,24 +47,28 @@ function RegisterLaunch() {
             });
     };
 
+    useEffect( () => {
+        
+    }, []);
+
     return (
         <Card title="Register Launch">
             <div className="row col-md-13">
                 <FormGroup htmlFor="inputDescription" label="* Description:">
-                    <input onChange={ event => setDescription(event.target.value) } id="inputDescription" type="text" className="form-control" />
+                    <input onChange={ event => setDescription(event.target.value) } id="inputDescription" type="text" className="form-control" placeholder={launchToBeUpdate.description} />
                 </FormGroup>
             </div>
 
             <div className="row">
                 <div className="col-md-6">
                     <FormGroup htmlFor="inputYear" label="* Year:">
-                        <input onChange={ event => setYear(event.target.value) } id="inputYear" type="text" className="form-control" />
+                        <input onChange={ event => setYear(event.target.value) } id="inputYear" type="text" className="form-control" placeholder={launchToBeUpdate.year} />
                     </FormGroup>
                 </div>
 
                 <div className="col-md-6">
                     <FormGroup htmlFor="inputMonth" label="* Month:">
-                        <MenuOption onChange={ event => setMonth(event.target.value) } id="inputMonth" options={monthsOptionList} className="form-control" />
+                        <MenuOption onChange={ event => setMonth(event.target.value) } id="inputMonth" options={monthsOptionList} className="form-control" placeholder={launchToBeUpdate.month} />
                     </FormGroup>
                 </div>
             </div>
@@ -69,19 +76,19 @@ function RegisterLaunch() {
             <div className="row">
                 <div className="col-md-4">
                     <FormGroup htmlFor="inputValue" label="* Value:">
-                        <input onChange={ event => setValue(event.target.value) } id="inputValue" type="text" className="form-control" />
+                        <input onChange={ event => setValue(event.target.value) } id="inputValue" type="text" className="form-control" placeholder={launchToBeUpdate.value} />
                     </FormGroup>
                 </div>
 
                 <div className="col-md-4">
                     <FormGroup htmlFor="inputType" label="* Type:">
-                        <MenuOption onChange={ event => setType(event.target.value) } id="inputType" options={launchTypesOptionList} className="form-control" />
+                        <MenuOption onChange={ event => setType(event.target.value) } id="inputType" options={launchTypesOptionList} className="form-control" placeholder={launchToBeUpdate.type} />
                     </FormGroup>
                 </div>
 
                 <div className="col-md-4">
                     <FormGroup htmlFor="inputStatus" label="Status:">
-                        <input id="inputStatus" type="text" className="form-control" disabled />
+                        <input id="inputStatus" type="text" className="form-control" placeholder={launchToBeUpdate.status} disabled />
                     </FormGroup>
                 </div>
             </div>
