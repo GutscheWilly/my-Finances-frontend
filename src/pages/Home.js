b4a2d3688f9c9f1b0051e06c91fd987766cf2d1b
Willy Gutsche <willy.gutsche@unifesp.br>
2023-04-18 22:30:09 -0300

    Include buttons to add or search launches in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,27 @@ import { useState, useEffect } from 'react';
 
 import UserService from '../service/user/UserService';
 import LocalStorageService from '../service/local-storage/LocalStorageService';
+import NavigateService from '../service/navigate/NavigateService.js'
+
+import { showErrorMessage } from '../components/Toastr';
+import icons from '../components/Icons';
 
 function Home() {
     const [balance, setBalance] = useState();
     const [userName, setUserName] = useState();
 
     const userService = new UserService();
+    const navigateService = NavigateService();
 
     const requestAndSetBalance = async (id) => {
         await userService.getBalance(id)
-            .then(response => {
+            .then( response => {
                 const requestedBalance = response.data;
                 setBalance(requestedBalance);
             })
-            .catch(error => {
-                console.log(error);
+            .catch( error => {
+                const errorMessage = error.response.data;
+                showErrorMessage(errorMessage);
             });
     };
 
@@ -33,9 +39,21 @@ function Home() {
     return (
         <div className="jumbotron">
             <h1 className="display-3">Welcome, {userName}!</h1>
-            <p className="lead">Your balance to this month is $ {balance}</p>
+            <p className="lead">Your current balance is: $ {balance}</p>
             <hr className="my-4"/>
             <p className="lead"></p>
+
+            <p className="lead">Here, you can add new launches or search by them</p>
+
+            <div className="mt-4"> 
+                <button title="Add a new Launch" onClick={navigateService.navigateToRegisterLaunch} className="btn btn-info btn-lg">
+                    {icons.add} Add launch
+                </button>
+
+                <button title="Search by Launches" onClick={navigateService.navigateToSearchLaunches} className="btn btn-success btn-lg">
+                    {icons.search} Search Launch
+                </button>
+            </div>
         </div>
     );
 }
