49d799e1c9a575201949b828da591a493050d029
Willy Gutsche <willy.gutsche@unifesp.br>
2023-04-24 22:57:57 -0300

    Include Protected Routes for personal pages

--- src/app/RoutesApp.js
+++ src/app/RoutesApp.js
@@ -7,15 +7,54 @@ import Home from '../pages/Home';
 import Launch from '../pages/launch/Launch';
 import RegisterLaunch from '../pages/launch/RegisterLaunch';
 
+import { Navigate } from 'react-router-dom';
+import LocalStorageService from '../service/local-storage/LocalStorageService';
+
+function ProtectedRoute({ children }) {
+    const isUserLogged = () => {
+        return LocalStorageService.getItem('logged_user') !== null;
+    };
+
+    if (isUserLogged()) {
+        return children;
+    }
+
+    return <Navigate to="/login" />
+}
+
 function RoutesApp() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={ <Login /> } />
                 <Route path="/register-user" element={ <RegisterUser /> } />
-                <Route path="/home" element={ <Home /> } />
-                <Route path="/launches" element={ <Launch /> } />
-                <Route path="/register-launch/:id?" element={ <RegisterLaunch /> } />
+
+                <Route 
+                    path="/home" 
+                    element={ 
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    } 
+                />
+
+                <Route 
+                    path="/launches" 
+                    element={ 
+                        <ProtectedRoute>
+                            <Launch />
+                        </ProtectedRoute>
+                    } 
+                />
+
+                <Route 
+                    path="/register-launch/:id?" 
+                    element={ 
+                        <ProtectedRoute>
+                            <RegisterLaunch />
+                        </ProtectedRoute>
+                    } 
+                />
             </Routes>
         </BrowserRouter>
     );
